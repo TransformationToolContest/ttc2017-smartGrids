f4254ad8d70b1a6400847aef4993a66bc50ed949
Georg Hinkel <hinkel@fzi.de>
2017-03-14 14:23:34 +0100

    First commit of generator and ModelJoin solution

---
+++ generator/Program.cs
@@ -0,0 +1,82 @@
+﻿using CommandLine;
+using NMF.Models;
+using NMF.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTC2017.SmartGrids.Generator
+{
+    public class Options
+    {
+        [Option("cim", HelpText = "The path to the cim model", Required = true)]
+        public string CimPath { get; set; }
+
+        [Option("cosem", HelpText = "The path to the cosem model", Required = true)]
+        public string CosemPath { get; set; }
+
+        [Option("substation", HelpText = "The path to the substation model", Required = true)]
+        public string SubstationPath { get; set; }
+
+        [Option("cimOut", HelpText = "The path to which the changed cim model should be saved to", Required = true)]
+        public string CimOutPath { get; set; }
+
+        [Option("cosemOut", HelpText = "The path to which the changed cosem model should be saved to", Required = true)]
+        public string CosemOutPath { get; set; }
+
+        [Option("substationOut", HelpText = "The path to which the changed substation model should be saved to", Required = true)]
+        public string SubstationOutPath { get; set; }
+
+        [Option('n', "changes", HelpText = "The number of changes to generate", Required = true)]
+        public int Changes { get; set; }
+    }
+
+    class Program
+    {
+        private Model cim;
+        private Model cosem;
+        private Model substation;
+        private Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            var options = new Options();
+            if (parser.ParseArguments(args, options))
+            {
+                var program = new Program();
+                program.GenerateChanges(options);
+                Console.WriteLine("Successfully generated changes");
+            }
+            else
+            {
+                Console.WriteLine("You are using me wrongly.");
+                Console.WriteLine(CommandLine.Text.HelpText.AutoBuild(options));
+            }
+        }
+
+        internal void GenerateChanges(Options options)
+        {
+            var repository = new ModelRepository();
+            cim = repository.Resolve(options.CimPath);
+            cosem = repository.Resolve(options.CosemPath);
+            substation = repository.Resolve(options.SubstationPath);
+
+            for (int i = 0; i < options.Changes; i++)
+            {
+                GenerateChanges();
+            }
+
+            repository.Save(cim, options.CimOutPath);
+            repository.Save(cosem, options.CosemOutPath);
+            repository.Save(substation, options.SubstationOutPath);
+        }
+
+        private void GenerateChanges()
+        {
+
+        }
+    }
+}
---
+++ generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using NMF.Models;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68ab91e3-84b6-4708-9cb7-c5036826db1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ModelMetadata("http://iec.ch/TC57/2009/CIM-schema-cim14", "TTC2017.SmartGrids.schema.nmf")]
+[assembly: ModelMetadata("http://www.transformation-tool-contest.eu/2017/smartGrids/substationStandard", "TTC2017.SmartGrids.61850.nmf")]
+[assembly: ModelMetadata("http://www.transformation-tool-contest.eu/2017/smartGrids/cosem", "TTC2017.SmartGrids.cosem.nmf")]
